fc140ca118dbdfcca936fd7be6e5b31ea1196476
Filipe Merker <tc.filipemerker@gruporbs.com.br>
2017-05-04 18:30:45 -0300

    Refactor api

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let axios = require('axios');
+let app = express();
+const api = {
+  earthquakes: {
+    get: (from, to) => axios.get(`http://earthquake.usgs.gov/fdsnws/event/1/count?format=geojson&starttime=${from}-01-01&endtime=${to}-12-31`)
+  }
+}
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,6 +18,17 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+getEarthquakes = (req, res, next) => {
+    let { from, to } = req.query;
+    api.earthquakes.get(from, to)
+      .then(response => res.json({ total: response.data.count}))
+      .catch(function (error) {
+        console.log(error);
+      });
+};
+
+app.get('/api/earthquakes', getEarthquakes);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
